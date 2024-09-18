24f4eda968aec1ef1b43d19d25103983f771c0e6
William1719 <william.lh1719@gmail.com>
2024-09-18 09:59:34 -0600

    Prueba1 de paypal

---
+++ server.js
@@ -0,0 +1,233 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Obtener el directorio actual del archivo
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888 } = process.env;
+const base = 'https://api-m.sandbox.paypal.com';
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+// parse post params sent in body in json format
+app.use(express.json());
+
+async function handleResponse(response) {
+    try {
+        const jsonResponse = await response.json();
+        return {
+            jsonResponse,
+            httpStatusCode: response.status,
+        };
+    } catch (err) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+}
+
+// serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+});
+
+/**
+ * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
+ * @see https://developer.paypal.com/api/rest/authentication/
+ */
+const generateAccessToken = async () => {
+    try {
+        if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+            throw new Error('MISSING_API_CREDENTIALS');
+        }
+        const auth = Buffer.from(
+            PAYPAL_CLIENT_ID + ':' + PAYPAL_CLIENT_SECRET
+        ).toString('base64');
+        const response = await fetch(`${base}/v1/oauth2/token`, {
+            method: 'POST',
+            body: 'grant_type=client_credentials',
+            headers: {
+                Authorization: `Basic ${auth}`,
+            },
+        });
+
+        const data = await response.json();
+        return data.access_token;
+    } catch (error) {
+        console.error('Failed to generate Access Token:', error);
+    }
+};
+
+/**
+ * Generate a client token for rendering the hosted card fields.
+ * @see https://developer.paypal.com/docs/checkout/advanced/integrate/#link-integratebackend
+ */
+const generateClientToken = async () => {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v1/identity/generate-token`;
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Accept-Language': 'en_US',
+            'Content-Type': 'application/json',
+        },
+    });
+
+    return handleResponse(response);
+};
+
+// return client token for hosted-fields component
+app.post('/api/token', async (req, res) => {
+    try {
+        const { jsonResponse, httpStatusCode } = await generateClientToken();
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error('Failed to generate client token:', error);
+        res.status(500).send({ error: 'Failed to generate client token.' });
+    }
+});
+
+/**
+ * Create an order to start the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+ */
+const createOrder = async (cart) => {
+    console.log(
+        'shopping cart information passed from the frontend createOrder() callback:',
+        cart
+    );
+
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+    const payload = {
+        intent: 'CAPTURE',
+        purchase_units: [
+            {
+                amount: {
+                    currency_code: 'USD',
+                    value: '100',
+                },
+            },
+        ],
+    };
+
+    const response = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+        },
+        method: 'POST',
+        body: JSON.stringify(payload),
+    });
+
+    return handleResponse(response);
+};
+
+app.post('/api/orders', async (req, res) => {
+    try {
+        const { cart } = req.body;
+        const { jsonResponse, httpStatusCode } = await createOrder(cart);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error('Failed to create order:', error);
+        res.status(500).json({ error: 'Failed to create order.' });
+    }
+});
+
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
+const captureOrder = async (orderID) => {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderID}/capture`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+
+    return handleResponse(response);
+};
+
+app.post('/api/orders/:orderID/capture', async (req, res) => {
+    try {
+        const { orderID } = req.params;
+        const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error('Failed to capture order:', error);
+        res.status(500).json({ error: 'Failed to capture order.' });
+    }
+});
+
+/**
+ * Authorize payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_authorize
+ */
+const authorizeOrder = async (orderID) => {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderID}/authorize`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+
+    return handleResponse(response);
+};
+
+app.post('/api/orders/:orderID/authorize', async (req, res) => {
+    try {
+        const { orderID } = req.params;
+        const { jsonResponse, httpStatusCode } = await authorizeOrder(orderID);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error('Failed to authorize order:', error);
+        res.status(500).json({ error: 'Failed to authorize order.' });
+    }
+});
+
+/**
+ * Captures an authorized payment, by ID.
+ * @see https://developer.paypal.com/docs/api/payments/v2/#authorizations_capture
+ */
+const captureAuthorize = async (authorizationId) => {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/payments/authorizations/${authorizationId}/capture`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+
+    return handleResponse(response);
+};
+
+app.post('/orders/:authorizationId/captureAuthorize', async (req, res) => {
+    try {
+        const { authorizationId } = req.params;
+        const { jsonResponse, httpStatusCode } = await captureAuthorize(authorizationId);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error('Failed to capture authorize:', error);
+        res.status(500).json({ error: 'Failed to capture authorize.' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Node server listening at http://localhost:${PORT}/`);
+});
